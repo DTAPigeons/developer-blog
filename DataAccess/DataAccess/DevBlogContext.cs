257aeb31f2056ae5c8e77f1170420b10fb99fcd5
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2019-08-14 15:24:56 +0300

    Added DbContext

---
+++ DataAccess/DataAccess/DevBlogContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+
+namespace DataAccess {
+    class DevBlogContext: DbContext {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Entity<UserEntity>().HasIndex(u => u.UserName).IsUnique();
+
+        }
+
+        public DevBlogContext() : base(){
+
+        }
+
+        DbSet<UserEntity> Users { get; set; }
+        DbSet<CommentEntity> Comments { get; set; }
+        DbSet<PostEntity> Posts { get; set; }
+        DbSet<ImageEntity> Images { get; set; }
+    }
+}
